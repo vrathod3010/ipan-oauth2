c5e67d3f9b711dd99f40b75a63aea5b8b12d83ae
vrathod3010 <48536022+vrathod3010@users.noreply.github.com>
2019-04-08 23:55:12 +1000

    Update passport-setup.js

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -18,56 +18,30 @@ passport.deserializeUser((id, done) => {
 
 passport.use(
     new GoogleStrategy({
-        // options for google strategy
-        clientID: keys.google.clientID,
-        clientSecret: keys.google.clientSecret,
-        callbackURL: 'https://oauth-ipan2.herokuapp.com/google/callback'
-    }, (accessToken, refreshToken, profile, done) => {
-        // check if user already exists in our own db
-        User.findOne({googleId: profile.sub}).then((currentUser) => {
+        //options for the google start
+        callbackURL : 'https://oauth-ipan2.herokuapp.com/google/callback',
+        clientID : keys.google.clientID,
+        clientSecret : keys.google.clientSecret
+    },(accessToken,refreshToken,profile,done)=>{
+        //passport callback function
+        console.log(profile.sub);
+
+        User.findOne({googleId: profile.sub}).then((currentUser)=>{
             if(currentUser){
-                // already have this user
-                console.log('user is: ', currentUser);
-                done(null, currentUser);
-            } else {
-                // if not, create user in our db
+                //already have the user
+                console.log('user is: ',currentUser);
+                done(null,currentUser);
+            }else{
+                //create a new user
                 new User({
-                    googleId: profile.sub
-                }).save().then((newUser) => {
-                    console.log('created new user: ', newUser);
-                    done(null, newUser);
+                    googleId : profile.sub
+                }).save().then((newUser) =>{
+                    console.log("new user",newUser);
+                    done(null,newUser);
                 });
             }
         });
     })
-);
-// passoprt.use(
-//     new GoogleStrategy({
-//         //options for the google start
-//         callbackURL : 'https://oauth-ipan2.herokuapp.com/google/callback',
-//         clientID : keys.google.clientID,
-//         clientSecret : keys.google.clientSecret
-//     },(accessToken,refreshToken,profile,done)=>{
-//         //passport callback function
-//         console.log(profile.sub);
-
-//         User.findOne({googleId: profile.pub}).then((currentUser)=>{
-//             if(currentUser){
-//                 //already have the user
-//                 console.log('user is: ',currentUser);
-//                 done(profile);
-//             }else{
-//                 //create a new user
-//                 new User({
-//                     googleId : profile.sub
-//                 }).save().then((newUser) =>{
-//                     console.log("new user",newUser);
-//                     done(null,newUser);
-//                 });
-//             }
-//         });
-     
-//     })
 
     
-// )
+)
