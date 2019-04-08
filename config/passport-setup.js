1666830cab4b2299895b05c979589b0b40e30bf3
vrathod3010 <48536022+vrathod3010@users.noreply.github.com>
2019-04-08 22:17:43 +1000

    Update passport-setup.js

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -16,9 +16,9 @@ passoprt.use(
             username : profile.name,
             googleId : profile.sub,
             emailId : profile.email
-        }).save().then((newUser)) =>{
-            console.log("new user created"+newUser);
-        }
+        }).save().then((newUser) =>{
+            console.log("new user created"+newUser)
+        })
     })
 
     
