128c0121380dcc35e437542615866f79bb2c37ef
vrathod3010 <48536022+vrathod3010@users.noreply.github.com>
2019-04-08 22:07:38 +1000

    checking email

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -11,7 +11,12 @@ passoprt.use(
         clientSecret : keys.google.clientSecret
     },(accessToken,refreshToken,profile,done)=>{
         //passport callback function
-        console.log(profile);
+        console.log(profile.email);
+        new User({
+            username : profile.name,
+            googleId : profile.sub,
+            emailId : profile.email
+        })
     })
 
     
