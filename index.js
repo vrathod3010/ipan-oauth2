db830ed1619ba7c67c5c4969a3b62c8c3cd7fa67
vrathod3010 <48536022+vrathod3010@users.noreply.github.com>
2019-04-08 20:27:50 +1000

    running

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
-var authRouter = require('./routes/auth-routes');
+
 
 
 
 app.set("view engine",'ejs');
 
-app.use('/auth',authRouter);
 
+app.get('/login',(req,res)=>{
+    res.render('login');
+});
 
+app.get('/google',(req,res)=>{
+    res.send("logging with google");
+});
+
+app.get('/logout',(req,res)=>{
+    res.send("logging out"); 
+});
 app.get("/",function(req,res){
     res.render("home");
 });
