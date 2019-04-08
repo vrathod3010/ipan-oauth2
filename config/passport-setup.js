f5183e930ebdbaa515e3ecd2b06cf69ce4112c53
vrathod3010 <48536022+vrathod3010@users.noreply.github.com>
2019-04-08 21:20:23 +1000

    Update passport-setup.js

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -4,7 +4,7 @@ const GoogleStrategy = require('passport-google-oauth20');
 passoprt.use(
     new GoogleStrategy({
         //options for the google start
-        callbackURL : '/google/callback',
+        callbackURL : 'https://oauth-ipan2.herokuapp.com/google/callback',
         clientID : '147888834235-0nm274au2jl9p20blfedfqb8q63o8rg5.apps.googleusercontent.com',
         clientSecret : 'w_C0BHDE4U1FJJ60ppJBPEQb'
     },()=>{
