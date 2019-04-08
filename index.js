b65ba4f21414650923f78c889296085c5587f8ca
vrathod3010 <48536022+vrathod3010@users.noreply.github.com>
2019-04-08 20:25:05 +1000

    routes and ejs

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
+var authRouter = require('./routes/auth-routes');
+
 
 
 app.set("view engine",'ejs');
+
+app.use('/auth',authRouter);
+
 
 app.get("/",function(req,res){
     res.render("home");
