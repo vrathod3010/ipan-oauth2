be6db5d4ec88d1676c4c5c5e7290d97b452377f3
vrathod3010 <48536022+vrathod3010@users.noreply.github.com>
2019-04-08 23:48:58 +1000

    new code

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -5,28 +5,42 @@ const User = require('../models/user-models');
 
 
 
-passport.use(new GoogleStrategy({
-    clientID: keys.google.clientID,
-    clientSecret: keys.google.clientSecret,
-    callbackURL: "https://oauth-ipan2.herokuapp.com/google/callback"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({ googleId: profile.sub }, function (err, user) {
-      return cb(err, user);
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then((user) => {
+        done(null, user);
     });
-  }
-));
+});
 
-// passoprt.serializeUser((user,done)=>{
-//     done(null,user.username);
-// });
 
-// passoprt.deserializeUser((name,done)=>{
-//     User.findById(name).then((user)=>{
-//         done(null,user.username);
-//     });
-// });
-
+passport.use(
+    new GoogleStrategy({
+        // options for google strategy
+        clientID: keys.google.clientID,
+        clientSecret: keys.google.clientSecret,
+        callbackURL: '/auth/google/redirect'
+    }, (accessToken, refreshToken, profile, done) => {
+        // check if user already exists in our own db
+        User.findOne({googleId: profile.sub}).then((currentUser) => {
+            if(currentUser){
+                // already have this user
+                console.log('user is: ', currentUser);
+                done(null, currentUser);
+            } else {
+                // if not, create user in our db
+                new User({
+                    googleId: profile.sub
+                }).save().then((newUser) => {
+                    console.log('created new user: ', newUser);
+                    done(null, newUser);
+                });
+            }
+        });
+    })
+);
 // passoprt.use(
 //     new GoogleStrategy({
 //         //options for the google start
@@ -35,9 +49,9 @@ passport.use(new GoogleStrategy({
 //         clientSecret : keys.google.clientSecret
 //     },(accessToken,refreshToken,profile,done)=>{
 //         //passport callback function
-//         console.log(profile.name.givenName);
+//         console.log(profile.sub);
 
-//         User.findOne({username: profile.name.givenName}).then((currentUser)=>{
+//         User.findOne({googleId: profile.pub}).then((currentUser)=>{
 //             if(currentUser){
 //                 //already have the user
 //                 console.log('user is: ',currentUser);
@@ -45,7 +59,6 @@ passport.use(new GoogleStrategy({
 //             }else{
 //                 //create a new user
 //                 new User({
-//                     username : profile.name.givenName,
 //                     googleId : profile.sub
 //                 }).save().then((newUser) =>{
 //                     console.log("new user",newUser);
