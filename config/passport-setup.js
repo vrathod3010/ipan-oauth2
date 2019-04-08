e2207635644055725d9f7d9d184c81f1f2ae0a72
vrathod3010 <48536022+vrathod3010@users.noreply.github.com>
2019-04-08 23:50:25 +1000

    Update passport-setup.js

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -21,7 +21,7 @@ passport.use(
         // options for google strategy
         clientID: keys.google.clientID,
         clientSecret: keys.google.clientSecret,
-        callbackURL: '/auth/google/redirect'
+        callbackURL: '/google/callback'
     }, (accessToken, refreshToken, profile, done) => {
         // check if user already exists in our own db
         User.findOne({googleId: profile.sub}).then((currentUser) => {
